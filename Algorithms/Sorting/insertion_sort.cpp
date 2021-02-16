2ea528193a8c4ab376038d2d9f14db29e4eeb801
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-02-16 12:41:36 +0530

    insertion sort algorithm

---
+++ Algorithms/Sorting/insertion_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    for (int i = 1; i < n; i++){
+        int current = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > current){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
